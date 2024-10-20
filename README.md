28fe9fcba063acd3b69001904b61cc8df7b2701b
Suresh61 <sureshabantika@gmail.com>
2024-10-20 20:59:19 +0530

    This is the admin panel for my portfolio website

---
+++ README.md
@@ -0,0 +1,23 @@
+# Portfolio Admin Panel
+
+This is the **admin panel** for my portfolio website. It allows me to **manage** and **update** the contents, images, and links displayed on the portfolio. With this panel, I can dynamically modify my personal and professional information, ensuring that the portfolio stays current and relevant.
+
+## Installation
+
+1. Clone the repository
+   
+   ```bash 
+   git clone https://github.com/Suresh1061/Portfolio_Admin_Panel.git
+   cd Portfolio_Admin_Panel
+   ```
+2. Install the dependencies
+
+   ```bash
+   pnpm install
+   ```
+
+3. Run the server
+
+   ```bash
+   pnpm run dev
+   ```
---
+++ src/types/index.ts
@@ -0,0 +1,98 @@
+export interface loginProps {
+    email: string;
+    password: string;
+}
+
+export interface infoProps {
+    $id: string;
+    name: string;
+    homeSubtitles: string;
+    homeDescription: string;
+    homeImg: string;
+    aboutImg: string;
+    aboutDescription: string;
+    resume: string;
+    github: string;
+    linkedin: string;
+    twitter: string;
+    facebook: string;
+    instagram: string;
+    mobile: string;
+    email: string;
+    shortAddress: string;
+    fullAddress: string;
+    leetcode?: string;
+    codeforces?: string;
+    hackerrank?: string;
+    codechef?: string;
+    geekforgeeks?: string;
+    codestudio?: string;
+}
+
+export interface educationProps {
+    $id: string;
+    course: string;
+    description: string;
+    duration: string;
+    result: string;
+    institute: string;
+    marks: string;
+}
+
+export interface projectProps {
+    $id: string;
+    imgSrc: string;
+    title: string;
+    description: string;
+    isBest: boolean;
+    best?: string | boolean;
+    tags: string;
+    category: string;
+    githubLink: string;
+    demoLink: string;
+}
+
+export interface SkillProps {
+    $id: string;
+    icon: string;
+    name: string;
+    category: string;
+    details: string;
+}
+
+export interface AchievementProps {
+    $id: string;
+    title: string;
+    description: string;
+    attachment: string;
+    fileLink: string;
+}
+
+export interface ExperienceProps {
+    $id: string;
+    title: string;
+    duration: string;
+    description: string;
+    place: string;
+    certificate: string;
+    offerLetter: string;
+    lor: string;
+    optional: string;
+    category: string;
+    techTools: string;
+    position: string;
+}
+
+export interface IReviews {
+    name: string;
+    position: string;
+    company: string;
+    linkedin: string;
+    review: string;
+    photo: string;
+}
+
+export interface ReviewProps extends IReviews {
+    $id: string;
+    isVerified: boolean;
+}
